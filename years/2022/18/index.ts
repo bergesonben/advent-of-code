ec5e49de4e2efa5c5441fa17cae79bbab9b3731e
Benjamin <benjamin@marq.com>
2022-12-17 23:11:08 -0700

    day 18 part 1

--- years/2022/18/index.ts
+++ years/2022/18/index.ts
@@ -12,8 +12,34 @@ const DAY = 18;
 // data path    : /home/benjamin/Documents/personal/advent-of-code/years/2022/18/data.txt
 // problem url  : https://adventofcode.com/2022/day/18
 
+type Coor = [number,number,number];
+
+function sharesSide(coor1: Coor, coor2: Coor): boolean {
+	let common = 0;
+	let diff;
+	for (let i = 0; i < 3; i++) {
+		if (coor1[i] == coor2[i]) common++;
+		else if (diff == undefined)	{
+			diff = Math.abs(coor1[i] - coor2[i])
+			if (diff > 1) return false;
+		} else return false;
+	}
+	return true;
+}
+
 async function p2022day18_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const droplets: [number,number,number][] = [];
+	let sharedSides = 0;
+	const lines = input.split("\n");
+	for (const line of lines) {
+		const curr: Coor = line.split(',').map(Number) as Coor;
+		for (let droplet of droplets) {
+			if (sharesSide(curr, droplet)) sharedSides++;
+		}
+		droplets.push(curr);
+	}
+
+	return (droplets.length * 6) - (sharedSides*2);
 }
 
 async function p2022day18_part2(input: string, ...params: any[]) {
@@ -21,7 +47,13 @@ async function p2022day18_part2(input: string, ...params: any[]) {
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
+	const part1tests: TestCase[] = [
+		{
+			input: `2,2,2\n1,2,2\n3,2,2\n2,1,2\n2,3,2\n2,2,1\n2,2,3\n2,2,4\n2,2,6\n1,2,5\n3,2,5\n2,1,5\n2,3,5`,
+			extraArgs: [],
+			expected: `64`
+		}
+	];
 	const part2tests: TestCase[] = [];
 
 	// Run tests
