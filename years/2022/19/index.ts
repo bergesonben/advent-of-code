0ebf180b144df919ab99834fec6c2eb214796e2c
Benjamin <benjamin@marq.com>
2022-12-19 21:19:33 -0700

    day 19 part 1 brute force doesn't work

--- years/2022/19/index.ts
+++ years/2022/19/index.ts
@@ -1,4 +1,4 @@
-import _ from "lodash";
+import _, { map } from "lodash";
 import * as util from "../../../util/util";
 import * as test from "../../../util/test";
 import chalk from "chalk";
@@ -12,8 +12,141 @@ const DAY = 19;
 // data path    : /home/benjamin/Documents/personal/advent-of-code/years/2022/19/data.txt
 // problem url  : https://adventofcode.com/2022/day/19
 
+enum Resource {
+	ore,
+	clay,
+	obsidian,
+	geode
+}
+type Cost = {
+	count: number,
+	type: Resource
+}
+type Robot = {
+	costs: Cost[],
+	outputType: Resource
+}
+type Blueprint = {
+	id: number,
+	oreRobot: Robot,
+	clayRobot: Robot,
+	obsidianRobot: Robot,
+	geodeRobot: Robot
+}
+
+type Inventory = Map<Resource, number>
+type Fleet = Map<Robot, number>
+
+type Snapshot = {
+	fleet: Fleet
+	inventory: Inventory
+}
+
+function parseBlueprint(line: string): Blueprint {
+	const tokens = line.split(' ');
+	const id = Number(_.trimEnd(tokens[1], ':'));
+	const oreRobotCost: Cost = {count: Number(tokens[6]), type: Resource.ore};
+	const oreRobot: Robot = {costs: [oreRobotCost], outputType: Resource.ore};
+	const clayRobotCost: Cost = {count: Number(tokens[12]), type: Resource.ore};
+	const clayRobot: Robot = {costs: [clayRobotCost], outputType: Resource.clay};
+	const obsidianRobotCosts: Cost[] = [{count: Number(tokens[18]), type: Resource.ore},{count: Number(tokens[21]), type: Resource.clay}];
+	const obsidianRobot: Robot = {costs: obsidianRobotCosts, outputType: Resource.obsidian};
+	const geodeRobotCosts: Cost[] = [{count: Number(tokens[27]), type: Resource.ore},{count: Number(tokens[30]), type: Resource.obsidian}];
+	const geodeRobot: Robot = {costs: geodeRobotCosts, outputType: Resource.geode};
+	const blueprint: Blueprint = {
+		id: id,
+		oreRobot: oreRobot,
+		clayRobot: clayRobot,
+		obsidianRobot: obsidianRobot,
+		geodeRobot: geodeRobot,
+	}
+	return blueprint;
+}
+
+function canBuildRobot(inventory: Inventory, robot: Robot): boolean {
+	for (let cost of robot.costs) {
+		if ((inventory.get(cost.type) ?? 0) < cost.count) return false;
+	}
+	return true;
+}
+
+function deepCopySnapshot(snapshot: Snapshot): Snapshot {
+	const newSnapshot = {} as Snapshot;
+	const inventoryCopy = new Map(snapshot.inventory);
+	const fleetCopy = new Map(snapshot.fleet);
+	newSnapshot.fleet = fleetCopy;
+	newSnapshot.inventory = inventoryCopy;
+	return newSnapshot;
+}
+
+function minutePassesBy(snapshot: Snapshot): Snapshot {
+	const newSnapshot = deepCopySnapshot(snapshot);
+	for (let robot of newSnapshot.fleet) {
+		const curr = newSnapshot.inventory.get(robot[0].outputType) ?? 0;
+		const newOutput = curr + robot[1];
+		newSnapshot.inventory.set(robot[0].outputType, newOutput);
+	}
+	return newSnapshot;
+}
+
+function buildRobot(snapshot: Snapshot, robot: Robot): void {
+	if (!canBuildRobot(snapshot.inventory, robot)) {
+		throw Error('cant build robot');
+	}
+	snapshot.fleet.set(robot, (snapshot.fleet.get(robot) ?? 0) + 1 );
+	for (let cost of robot.costs) {
+		snapshot.inventory.set(cost.type, snapshot.inventory.get(cost.type)! - cost.count);
+	}
+}
+
+function choices(snapshot: Snapshot): Snapshot[] {
+	const newPaths: Snapshot[] = [minutePassesBy(snapshot)];
+	
+	for (let robot of snapshot.fleet) {
+		if (canBuildRobot(snapshot.inventory, robot[0])) {			
+			let newSnapshot = minutePassesBy(snapshot);
+			buildRobot(newSnapshot, robot[0]);			
+			newPaths.push(newSnapshot);
+		}
+	}
+	return newPaths;
+}
+
+function maxGeodeOutput(blueprint: Blueprint, time: number): number {
+	const inventory: Inventory = new Map();
+	const fleet: Fleet = new Map();	
+	for (let i = 0; i < 4; i++) {
+		inventory.set(i as Resource, 0)
+	}
+	fleet.set(blueprint.oreRobot, 1);
+	fleet.set(blueprint.clayRobot, 0);
+	fleet.set(blueprint.obsidianRobot, 0);
+	fleet.set(blueprint.geodeRobot, 0);
+	const snapshot = {
+		inventory: inventory,
+		fleet: fleet
+	}
+
+	let snapshots: Snapshot[] = [snapshot];
+	for (let i = 0; i < 24; i++) {
+		let newSnapshots: Snapshot[] = [];
+		for (let snapshot of snapshots) {
+			newSnapshots = newSnapshots.concat(choices(snapshot));
+		}
+		snapshots = newSnapshots;
+	}
+
+	return 0;
+}
+
 async function p2022day19_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const blueprints: Blueprint[] = [];
+	const lines = input.split("\n");
+	for (const line of lines) {
+		blueprints.push(parseBlueprint(line))
+	}
+	const outputs = blueprints.map(x => maxGeodeOutput(x, 24) * x.id);
+	return outputs.reduce((p,c) => p + c);
 }
 
 async function p2022day19_part2(input: string, ...params: any[]) {
@@ -21,7 +154,13 @@ async function p2022day19_part2(input: string, ...params: any[]) {
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
+	const part1tests: TestCase[] = [
+		{
+			input: `Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.\nBlueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.`,
+			extraArgs: [],
+			expected: `33`
+		}
+	];
 	const part2tests: TestCase[] = [];
 
 	// Run tests
