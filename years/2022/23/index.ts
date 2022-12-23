4d5bedaf99a91b3a7bf32cea2b5d06c1d54d4afc
Benjamin <benjamin@marq.com>
2022-12-22 23:03:50 -0700

    day 23 part 2

--- years/2022/23/index.ts
+++ years/2022/23/index.ts
@@ -29,6 +29,9 @@ const SOUTHERN: Direction[] = [S, SW, SE];
 const EASTERN: Direction[] = [E, SE, NE];
 const WESTERN: Direction[] = [W,SW,NW];
 
+function rotateDirections(directions: Direction[][]): void {
+	directions.push(directions.shift()!);
+}
 
 async function p2022day23_part1(input: string, ...params: any[]) {
 	let dirs: Direction[][] = [NORTHERN, SOUTHERN, WESTERN, EASTERN];
@@ -113,7 +116,7 @@ async function p2022day23_part1(input: string, ...params: any[]) {
 			elves.add(destination);
 		}
 
-		rotateDirs();
+		rotateDirections(dirs);
 	}
 
 	function proposeMove(elf: Coor): string | null {
@@ -146,8 +149,125 @@ async function p2022day23_part1(input: string, ...params: any[]) {
 		return false;
 	}
 
-	function rotateDirs(): void {
-		dirs.push(dirs.shift()!);
+	function getSmallestRectArea(): number {
+		let [rowMin, rowMax, colMin, colMax] = getNSEWMaxes();
+		const width = rowMax - rowMin + 1;
+		const height = colMax - colMin + 1;
+		return width * height;
+	}
+}
+
+async function p2022day23_part2(input: string, ...params: any[]) {
+	let dirs: Direction[][] = [NORTHERN, SOUTHERN, WESTERN, EASTERN];
+	const elves: Set<string> = new Set(); // strigified Coors	
+	initElves();
+	// console.log("START");
+	// printMap();
+	let rounds = 1;
+	while (round()) {
+		rounds++;
+	}
+	return rounds;
+
+	function initElves(): void {
+		const lines = input.split("\n");
+		for (const [row, line] of lines.entries()) {
+			for (let col = 0; col < line.length; col++) {
+				const cell = line[col];
+				if (cell == '#') {
+					elves.add([row,col].toString());
+				}
+			}			
+		}
+	}
+
+	function printMap(): void {
+		let [rowMin, rowMax, colMin, colMax] = getNSEWMaxes();
+		for (let row = rowMin;row < rowMax + 1; row++) {
+			let line = '';
+			for (let col = colMin; col < colMax + 1; col++) {
+				if (elves.has([row,col].toString())) {
+					line += '#';
+				} else {
+					line += '.';
+				}
+			}
+			console.log(line);
+		}
+	}
+
+	function getNSEWMaxes(): [number,number,number,number] {
+		let minRow = Number.MAX_SAFE_INTEGER;
+		let maxRow = Number.MIN_SAFE_INTEGER;
+		let minCol = Number.MAX_SAFE_INTEGER;
+		let maxCol = Number.MIN_SAFE_INTEGER;
+		
+		for (let elfStr of elves) {
+			const elf = decode(elfStr);
+			minRow = Math.min(minRow, elf[0]);
+			maxRow = Math.max(maxRow, elf[0]);
+			minCol = Math.min(minCol, elf[1]);
+			maxCol = Math.max(maxCol, elf[1]);
+		}		
+		return [minRow, maxRow, minCol, maxCol];
+	}
+
+	function round(): boolean {
+		const validMoves: Map<string, string> = new Map();
+		const invalidMoves: Set<string> = new Set();
+		for (let elfStr of elves.keys()) {
+			const elf = decode(elfStr);
+			if (needToMove(elf)) {
+				const proposed = proposeMove(elf);
+				if (proposed == null) continue;
+				if (validMoves.has(proposed)) {
+					invalidMoves.add(proposed);
+					validMoves.delete(proposed);
+				} else if (!invalidMoves.has(proposed)) {
+					validMoves.set(proposed, elfStr);
+				}
+			}
+		}
+
+		if (validMoves.size == 0) {return false;}
+
+		for (let [destination, origin] of validMoves.entries()) {
+			elves.delete(origin);
+			elves.add(destination);
+		}
+
+		rotateDirections(dirs);
+		return true;
+	}
+
+	function proposeMove(elf: Coor): string | null {
+		for (let generalDir of dirs) {
+			let valid = true;
+			for (let specificDir of generalDir) {
+				const newCoor = [elf[0] + specificDir[0], elf[1] + specificDir[1]];
+				if (elves.has(newCoor.toString())) {
+					valid = false;
+					break;
+				}
+			}
+			if (valid) {
+				const newCoor = [elf[0] + generalDir[0][0], elf[1] + generalDir[0][1]];
+				return newCoor.toString();
+			}
+		}
+		return null;
+	}
+
+	function decode(str: string): Coor {
+		return str.split(',').map(Number) as Coor;
+	}
+	
+	function needToMove(elf: Coor): boolean {
+		for (let dir of ALL_8_DIRECTIONS) {
+			const newCoor: Coor = [elf[0] + dir[0], elf[1] + dir[1]];
+			if (elves.has(newCoor.toString())) return true;
+		}
+		return false;
 	}
 
 	function getSmallestRectArea(): number {
@@ -158,10 +278,6 @@ async function p2022day23_part1(input: string, ...params: any[]) {
 	}
 }
 
-async function p2022day23_part2(input: string, ...params: any[]) {
-	return "Not implemented";
-}
-
 async function run() {
 	const part1tests: TestCase[] = [
 		{
@@ -170,7 +286,13 @@ async function run() {
 			expected: `110`
 		}
 	];
-	const part2tests: TestCase[] = [];
+	const part2tests: TestCase[] = [
+		{
+			input: `....#..\n..###.#\n#...#.#\n.#...##\n#.###..\n##.#.##\n.#..#..`,
+			extraArgs: [],
+			expected: `20`
+		}
+	];
 
 	// Run tests
 	test.beginTests();
@@ -187,22 +309,22 @@ async function run() {
 	test.endTests();
 
 	// Get input and run program while measuring performance
-	const input = await util.getInput(DAY, YEAR);
+	// const input = await util.getInput(DAY, YEAR);
 
-	const part1Before = performance.now();
-	const part1Solution = String(await p2022day23_part1(input));
-	const part1After = performance.now();
+	// const part1Before = performance.now();
+	// const part1Solution = String(await p2022day23_part1(input));
+	// const part1After = performance.now();
 
-	const part2Before = performance.now()
-	const part2Solution = String(await p2022day23_part2(input));
-	const part2After = performance.now();
+	// const part2Before = performance.now()
+	// const part2Solution = String(await p2022day23_part2(input));
+	// const part2After = performance.now();
 
-	logSolution(23, 2022, part1Solution, part2Solution);
+	// logSolution(23, 2022, part1Solution, part2Solution);
 
-	log(chalk.gray("--- Performance ---"));
-	log(chalk.gray(`Part 1: ${util.formatTime(part1After - part1Before)}`));
-	log(chalk.gray(`Part 2: ${util.formatTime(part2After - part2Before)}`));
-	log();
+	// log(chalk.gray("--- Performance ---"));
+	// log(chalk.gray(`Part 1: ${util.formatTime(part1After - part1Before)}`));
+	// log(chalk.gray(`Part 2: ${util.formatTime(part2After - part2Before)}`));
+	// log();
 }
 
 run()
