0a88b67d05a66a422cdf4d27711ce1765f68e3b2
Benjamin <benjamin@marq.com>
2022-12-22 22:55:22 -0700

    day 23 part 1

--- years/2022/23/index.ts
+++ years/2022/23/index.ts
@@ -1,4 +1,4 @@
-import _ from "lodash";
+import _, { round } from "lodash";
 import * as util from "../../../util/util";
 import * as test from "../../../util/test";
 import chalk from "chalk";
@@ -12,8 +12,150 @@ const DAY = 23;
 // data path    : /home/benjamin/Documents/personal/advent-of-code/years/2022/23/data.txt
 // problem url  : https://adventofcode.com/2022/day/23
 
+type Coor = [number,number];
+
+type Direction = [number,number];
+const N: Direction = [-1,0];
+const W: Direction = [0,-1];
+const S: Direction = [1,0];
+const E: Direction = [0,1];
+const NW: Direction = [-1,-1];
+const NE: Direction = [-1, 1];
+const SW: Direction = [1, -1];
+const SE: Direction = [1, 1];
+const ALL_8_DIRECTIONS: Direction[] = [N,W,S,E,NW,NE,SW,SE];
+const NORTHERN: Direction[] = [N, NW, NE];
+const SOUTHERN: Direction[] = [S, SW, SE];
+const EASTERN: Direction[] = [E, SE, NE];
+const WESTERN: Direction[] = [W,SW,NW];
+
+
 async function p2022day23_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	let dirs: Direction[][] = [NORTHERN, SOUTHERN, WESTERN, EASTERN];
+	const elves: Set<string> = new Set(); // strigified Coors	
+	initElves();
+	// console.log("START");
+	// printMap();
+	const ROUNDS = 10;
+	for (let i = 0; i < ROUNDS; i++) {
+		round();
+	}
+	// console.log('After 1 round');
+	// printMap();
+	
+	const area = getSmallestRectArea();
+	const elvesCount = elves.size;
+	const answer = area - elvesCount;
+	return answer;
+
+	function initElves(): void {
+		const lines = input.split("\n");
+		for (const [row, line] of lines.entries()) {
+			for (let col = 0; col < line.length; col++) {
+				const cell = line[col];
+				if (cell == '#') {
+					elves.add([row,col].toString());
+				}
+			}			
+		}
+	}
+
+	function printMap(): void {
+		let [rowMin, rowMax, colMin, colMax] = getNSEWMaxes();
+		for (let row = rowMin;row < rowMax + 1; row++) {
+			let line = '';
+			for (let col = colMin; col < colMax + 1; col++) {
+				if (elves.has([row,col].toString())) {
+					line += '#';
+				} else {
+					line += '.';
+				}
+			}
+			console.log(line);
+		}
+	}
+
+	function getNSEWMaxes(): [number,number,number,number] {
+		let minRow = Number.MAX_SAFE_INTEGER;
+		let maxRow = Number.MIN_SAFE_INTEGER;
+		let minCol = Number.MAX_SAFE_INTEGER;
+		let maxCol = Number.MIN_SAFE_INTEGER;
+		
+		for (let elfStr of elves) {
+			const elf = decode(elfStr);
+			minRow = Math.min(minRow, elf[0]);
+			maxRow = Math.max(maxRow, elf[0]);
+			minCol = Math.min(minCol, elf[1]);
+			maxCol = Math.max(maxCol, elf[1]);
+		}		
+		return [minRow, maxRow, minCol, maxCol];
+	}
+
+	function round(): void {
+		const validMoves: Map<string, string> = new Map();
+		const invalidMoves: Set<string> = new Set();
+		for (let elfStr of elves.keys()) {
+			const elf = decode(elfStr);
+			if (needToMove(elf)) {
+				const proposed = proposeMove(elf);
+				if (proposed == null) continue;
+				if (validMoves.has(proposed)) {
+					invalidMoves.add(proposed);
+					validMoves.delete(proposed);
+				} else if (!invalidMoves.has(proposed)) {
+					validMoves.set(proposed, elfStr);
+				}
+			}
+		}
+
+		for (let [destination, origin] of validMoves.entries()) {
+			elves.delete(origin);
+			elves.add(destination);
+		}
+
+		rotateDirs();
+	}
+
+	function proposeMove(elf: Coor): string | null {
+		for (let generalDir of dirs) {
+			let valid = true;
+			for (let specificDir of generalDir) {
+				const newCoor = [elf[0] + specificDir[0], elf[1] + specificDir[1]];
+				if (elves.has(newCoor.toString())) {
+					valid = false;
+					break;
+				}
+			}
+			if (valid) {
+				const newCoor = [elf[0] + generalDir[0][0], elf[1] + generalDir[0][1]];
+				return newCoor.toString();
+			}
+		}
+		return null;
+	}
+
+	function decode(str: string): Coor {
+		return str.split(',').map(Number) as Coor;
+	}
+	
+	function needToMove(elf: Coor): boolean {
+		for (let dir of ALL_8_DIRECTIONS) {
+			const newCoor: Coor = [elf[0] + dir[0], elf[1] + dir[1]];
+			if (elves.has(newCoor.toString())) return true;
+		}
+		return false;
+	}
+
+	function rotateDirs(): void {
+		dirs.push(dirs.shift()!);
+	}
+
+	function getSmallestRectArea(): number {
+		let [rowMin, rowMax, colMin, colMax] = getNSEWMaxes();
+		const width = rowMax - rowMin + 1;
+		const height = colMax - colMin + 1;
+		return width * height;
+	}
 }
 
 async function p2022day23_part2(input: string, ...params: any[]) {
@@ -21,7 +163,13 @@ async function p2022day23_part2(input: string, ...params: any[]) {
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
+	const part1tests: TestCase[] = [
+		{
+			input: `....#..\n..###.#\n#...#.#\n.#...##\n#.###..\n##.#.##\n.#..#..`,
+			extraArgs: [],
+			expected: `110`
+		}
+	];
 	const part2tests: TestCase[] = [];
 
 	// Run tests
