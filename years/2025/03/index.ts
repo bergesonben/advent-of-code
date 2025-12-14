12db7451f163950417b748df9cce3996189a7d17
Benjamin <benjamin@marq.com>
2025-12-13 18:33:36 -0700

    2025 Day 3

--- years/2025/03/index.ts
+++ years/2025/03/index.ts
@@ -1,4 +1,4 @@
-import _ from "lodash";
+import _, { max } from "lodash";
 import * as util from "../../../util/util";
 import * as test from "../../../util/test";
 import chalk from "chalk";
@@ -15,15 +15,74 @@ const DAY = 3;
 
 async function p2025day3_part1(input: string, ...params: any[]) {
 	return "Not implemented";
+	let total = 0;
+	for (const line of input.split("\n")) {
+		let maxFirst = -1;
+		let maxSecond = -1;
+		for (let i = 0; i < line.length - 1; i++) {
+			const digit = parseInt(line[i], 10);
+			if (digit > maxFirst) {
+				maxFirst = digit;
+				maxSecond = parseInt(line[i + 1], 10);
+			} else if (digit > maxSecond) {
+				maxSecond = digit;
+			}
+		}
+		const lastDigit = parseInt(line[line.length-1], 10);
+		if (lastDigit > maxSecond) {
+			maxSecond = lastDigit;
+		}
+		// console.log(`Line: ${line} => Max Jolt: ${maxFirst}${maxSecond}`);
+		const maxJolt = maxFirst * 10 + maxSecond;
+		total += maxJolt;
+	}
+	return total;
 }
 
 async function p2025day3_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	let total = 0;
+	for (const line of input.split("\n")) {
+		const maxArray: number[] = new Array(12).fill(-1);	
+		for (let i = 0; i < line.length; i++) {			
+			const digit = parseInt(line[i], 10);
+			let reset = false;
+			const foo = line.length - i;
+			const bar = 12 - foo;
+			const baz = Math.max(0, bar);
+			for (let j = baz; j < 12; j++) {
+				if (reset) {
+					maxArray[j] = -1;
+				} else if (digit > maxArray[j]) {
+					maxArray[j] = digit;
+					reset = true;
+				}
+			}			
+			// console.log(`After processing digit ${digit} => ${maxArray}`);
+		}
+		let maxJoltStr = '';
+		for (let i = 0; i < 12; i++) {
+			maxJoltStr += String(maxArray[i]);
+		}
+		const maxJolt = parseInt(maxJoltStr, 10);
+		// console.log(`Line: ${line} => Max Jolt: ${maxJoltStr}`);
+		total += maxJolt;
+	}
+	return total;
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
-	const part2tests: TestCase[] = [];
+	const part1tests: TestCase[] = [
+// 		{ input: `987654321111111
+// 811111111111119
+// 234234234234278
+// 818181911112111`, expected: `357` },
+	];
+	const part2tests: TestCase[] = [
+// 				{ input:  `987654321111111
+// 811111111111119
+// 234234234234278
+// 818181911112111`, expected: `3121910778619` },
+	];
 
 	const [p1testsNormalized, p2testsNormalized] = normalizeTestCases(part1tests, part2tests);
 
@@ -42,6 +101,7 @@ async function run() {
 	test.endTests();
 
 	// Get input and run program while measuring performance
+	
 	const input = await util.getInput(DAY, YEAR);
 
 	const part1Before = performance.now();
